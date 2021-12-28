36bece789cd9c2b68b505b61a0b282ca9515c35e
chengrong.liu <chengrong.liu@i-tudou.com>
2021-12-28 10:40:17 +0800

    模型样式修改功能开发

--- cesium-example/src/components/3dtiles/3DTilesStyle.vue
+++ cesium-example/src/components/3dtiles/3DTilesStyle.vue
@@ -10,6 +10,7 @@
           default-value="Color By Building Material"
           style="width: 250px; margin-right: 10px"
           size="small"
+          @change="changeStyle"
         >
           <a-select-option
             v-for="(item, index) in selectData"
@@ -61,9 +62,7 @@ export default {
     //去除版权信息
     this.viewer._cesiumWidget._creditContainer.style.display = "none";
     // 注册事件处理器
-    // const handler = new Cesium.ScreenSpaceEventHandler(
-    //   this.viewer.scene.canvas
-    // );
+    this.handler = new Cesium.ScreenSpaceEventHandler(this.viewer.scene.canvas);
     // 添加3dtiles
     this.add3dtiles();
     // 设置初始化视角
@@ -82,15 +81,19 @@ export default {
       // 添加osm build
       this.osmBuildingsTileset = Cesium.createOsmBuildings();
       this.viewer.scene.primitives.add(this.osmBuildingsTileset);
+      this.changeStyle("Color By Building Material");
     },
-    changeStyle() {},
+    changeStyle(type) {
+      const style = this.getStyleByType(type);
+      this.osmBuildingsTileset.style = new Cesium.Cesium3DTileStyle(style);
+    },
     getStyleByType(type) {
       let style = "";
       switch (type) {
         case "Color By Building Material":
           style = {
             defines: {
-              material: "${feature['building: material']}"
+              material: "${feature['building:material']}"
             },
             color: {
               conditions: [
@@ -101,17 +104,86 @@ export default {
                 ["${material} === 'stone'", "color('lightslategrey')"],
                 ["${material} === 'metal'", "color('lightgrey')"],
                 ["${material} === 'steel'", "color('lightsteelblue')"],
-                ["true", "color('white')"] // 上述条件都不符合时，走这里
+                ["true", "color('white')"] // This is the else case
               ]
             }
           };
           break;
         case "Color By Distance To Selected Location":
+          // 默认选取一个点
+          style = this.colorByDistanceToCoordinate(47.62051, -122.34931);
+          //注册鼠标点击事件
+          this.clickEvent();
+          break;
+        case "Highlight Residential Buildings":
+          style = {
+            color: {
+              conditions: [
+                [
+                  "${feature['building']} === 'apartments' || ${feature['building']} === 'residential'",
+                  "color('cyan', 0.9)"
+                ],
+                ["true", "color('white')"]
+              ]
+            }
+          };
+          break;
+        case "Show Office Buildings Only":
+          style = {
+            show: "${feature['building']} === 'office'"
+          };
+          break;
+        case "Show Apartment Buildings Only":
+          style = {
+            show: "${feature['building']} === 'apartments'"
+          };
           break;
         default:
           break;
       }
       return style;
+    },
+    clickEvent() {
+      const self = this;
+      this.handler.setInputAction(movement => {
+        // 点击后取消viewer追踪的entity，即不显示infobox弹框
+        self.viewer.selectedEntity = undefined;
+        const pickedBuilding = self.viewer.scene.pick(movement.position);
+        if (pickedBuilding) {
+          const pickedLatitude = pickedBuilding.getProperty("cesium#latitude");
+          const pickedLongitude = pickedBuilding.getProperty(
+            "cesium#longitude"
+          );
+          const style = self.colorByDistanceToCoordinate(
+            pickedLatitude,
+            pickedLongitude
+          );
+          self.osmBuildingsTileset.style = new Cesium.Cesium3DTileStyle(style);
+        }
+      }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+    },
+    // 根据距离点位的距离模型显示不同的颜色
+    colorByDistanceToCoordinate(pickedLatitude, pickedLongitude) {
+      let result = {
+        defines: {
+          distance:
+            "distance(vec2(${feature['cesium#longitude']}, ${feature['cesium#latitude']}), vec2(" +
+            pickedLongitude +
+            "," +
+            pickedLatitude +
+            "))"
+        },
+        color: {
+          conditions: [
+            ["${distance} > 0.014", "color('blue')"],
+            ["${distance} > 0.010", "color('green')"],
+            ["${distance} > 0.006", "color('yellow')"],
+            ["${distance} > 0.0001", "color('red')"],
+            ["true", "color('white')"]
+          ]
+        }
+      };
+      return result;
     }
   }
 };
